5936ddc37f86b9a9ae6f13f93dc6ecb098c597e5
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-05-19 13:44:18 -0400

    Update assembly informational version for EjercicioPrueba

--- EjercicioPrueba/obj/Debug/net8.0/EjercicioPrueba.AssemblyInfo.cs
+++ EjercicioPrueba/obj/Debug/net8.0/EjercicioPrueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3403fb12ea173b79900eee1c4144bcc14acc4a87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289f158051a2b34b3204785c01144776104ff1b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
