a1d2d8fdd214d461c25f5d875403d8ba233eb49c
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-05-19 13:44:51 -0400

    Update assembly informational version for ProblemasVarios

--- ProblemasVarios/obj/Debug/net8.0/ProblemasVarios.AssemblyInfo.cs
+++ ProblemasVarios/obj/Debug/net8.0/ProblemasVarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProblemasVarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a425392e8bbe4b2888ee85a6d5573261961e7116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289f158051a2b34b3204785c01144776104ff1b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProblemasVarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProblemasVarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
