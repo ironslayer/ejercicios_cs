b8daa851dc1d49b386176ae92698368aea9cf6f7
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-05-20 18:01:36 -0400

    Update assembly informational versions and cache files for EjercicioPrueba, EjerciciosMatrices, EjerciciosVectores, and ProblemasVarios

--- EjerciciosVectores/obj/Debug/net10.0/EjerciciosVectores.AssemblyInfo.cs
+++ EjerciciosVectores/obj/Debug/net10.0/EjerciciosVectores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjerciciosVectores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289f158051a2b34b3204785c01144776104ff1b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44edac884183d3e8849695550f9bf4ad31584293")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjerciciosVectores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjerciciosVectores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
