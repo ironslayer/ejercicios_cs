2bf5c4425398d9ecc4893abbb40627f872317dcd
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-05-21 15:45:09 -0400

    Update assembly informational version for EjerciciosMatrices

--- EjerciciosMatrices/obj/Debug/net10.0/EjerciciosMatrices.AssemblyInfo.cs
+++ EjerciciosMatrices/obj/Debug/net10.0/EjerciciosMatrices.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjerciciosMatrices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44edac884183d3e8849695550f9bf4ad31584293")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8daa851dc1d49b386176ae92698368aea9cf6f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjerciciosMatrices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjerciciosMatrices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
