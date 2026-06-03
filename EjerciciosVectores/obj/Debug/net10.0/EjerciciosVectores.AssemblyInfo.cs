8f2902b0a1b6b290f6bfeedda5bd79d0e6b62d5b
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-06-03 18:01:07 -0400

    update AssemblyInformationalVersion attribute with new hash

--- EjerciciosVectores/obj/Debug/net10.0/EjerciciosVectores.AssemblyInfo.cs
+++ EjerciciosVectores/obj/Debug/net10.0/EjerciciosVectores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjerciciosVectores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc6df96886be5ec0d7e7631fb009cc76342c9df5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d5045c6395f2ca02fb55d894b903fe0597faae")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjerciciosVectores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjerciciosVectores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
