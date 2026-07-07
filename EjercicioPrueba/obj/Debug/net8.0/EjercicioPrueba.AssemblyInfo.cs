4332f805e9cd7c8d9d31cc11a2bbea551c211ca0
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-07-07 05:01:36 -0400

    update AssemblyInformationalVersion attribute with new hash

--- EjercicioPrueba/obj/Debug/net8.0/EjercicioPrueba.AssemblyInfo.cs
+++ EjercicioPrueba/obj/Debug/net8.0/EjercicioPrueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24342912542e869aae0301e400f45151c308af7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51643ebbfbdda400b747a1fb982b3eafeba3c268")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
