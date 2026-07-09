9aa7550269b5dc0463694e5a69a6d64c50ab7b8b
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-07-09 07:50:49 -0400

    update AssemblyInformationalVersion attribute with new hash

--- ProblemasVarios/obj/Debug/net8.0/ProblemasVarios.AssemblyInfo.cs
+++ ProblemasVarios/obj/Debug/net8.0/ProblemasVarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProblemasVarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884fac01c4169093eea180f80c24c516b8545a81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87e85e724f5ef5265e2974e7e291e397b0ded1af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProblemasVarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProblemasVarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
