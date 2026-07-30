6ae749377132bd2038dd0d4b6d167b597f4e0b26
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-07-30 15:08:28 -0400

    update AssemblyInformationalVersion hash in EjerciciosMatrices assembly info

--- EjerciciosMatrices/obj/Debug/net10.0/EjerciciosMatrices.AssemblyInfo.cs
+++ EjerciciosMatrices/obj/Debug/net10.0/EjerciciosMatrices.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjerciciosMatrices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26533456d671a916fc61ffdef08cf5e5769a2ddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9fe0b700af9871682829c01503ad76540bcb5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjerciciosMatrices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjerciciosMatrices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
