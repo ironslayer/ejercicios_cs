7e4caa71b260cc1c332dd4a077afa5d6441c98ed
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-08-24 12:53:31 -0400

    update AssemblyInformationalVersion to latest hash and clean up unnecessary blank lines

--- EjercicioPrueba/obj/Debug/net8.0/EjercicioPrueba.AssemblyInfo.cs
+++ EjercicioPrueba/obj/Debug/net8.0/EjercicioPrueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0ec6dad213d2faf8f0f4090d95cb52b89b8084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315b80387414f08f359228a02221a0c4eacd5098")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
