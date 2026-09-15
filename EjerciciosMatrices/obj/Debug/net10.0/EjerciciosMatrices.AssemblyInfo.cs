035fc86145b615f0964ebf2c6a11fbb132945f16
alvaro <alvaro.guarachi.ticona@gmail.com>
2026-09-15 17:41:42 -0400

    update AssemblyInformationalVersion to latest hash

--- EjerciciosMatrices/obj/Debug/net10.0/EjerciciosMatrices.AssemblyInfo.cs
+++ EjerciciosMatrices/obj/Debug/net10.0/EjerciciosMatrices.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjerciciosMatrices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef43da783373433e3cb84dcc005434709ea8bcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c82d0df6c29e33af7c0dd9d2a198c939fbaa671")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjerciciosMatrices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjerciciosMatrices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
